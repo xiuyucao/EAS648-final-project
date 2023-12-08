c116e2278f50800ddcb8b4c39d7b1f2e9ee56db6
Xiuyu Cao <xiuyucao@umich.edu>
2023-12-08 15:14:12 -0500

    Completed rudimentary UI

--- app.R
+++ app.R
@@ -1,23 +1,52 @@
 library(shiny)
 library(shinydashboard)
-library(markdown)
+library(leaflet)  # for plotting maps
+library(markdown)  # for inserting markdown files
+
+# ----------------------------------------------- Data ----------------------------------------------- #
+
 
 # ------------------------------------------------ UI ------------------------------------------------ #
 ui <- navbarPage('Temperature Comparison',
                  tabPanel('Introduction',
                           fluidRow(
-                            column(6,
-                                   h1('This is Introduction')),
-                            column(3,
-                                   h1('Here is an image'),
-                                   tags$small(
-                                     "Source: xxxxxx ",
-                                     "bbbbbb ",
-                                     "ccccc ",
-                                     a(href="http://commons.wikimedia.org/wiki/User:Sfoskett",
-                                       "User:Sfoskett"))))),
-                 tabPanel('Data Explorer'),
-                 tabPanel('Visualization')
+                            column(5, includeMarkdown('intro.md')),
+                            column(3,img(src = 'daymet.png', width='100%'),
+                                   img(src = 'landsat.png', width='150%'),
+                                   img(src='nlcd.png', width='150%')))
+                          ),
+                 tabPanel('Data Explorer',
+                          sidebarPanel(
+                            helpText('Preprocessing includes interpolation, reprojection, and polygon generation.'),
+                            radioButtons('dataLevel', 'Data Level', c('Raw', 'Preprocessed'), selected='Raw'),
+                            
+                            conditionalPanel(condition = "input.dataLevel == 'Raw'",
+                              selectInput('varRaw', 'Variable', c('Land Cover Type', 'Daymet', 'LST'), selected='Land Cover Type'),
+                              conditionalPanel(condition= "input.varRaw != 'Land Cover Type'",
+                                               numericInput('month', 'Month', min=1, max=11, value=7),
+                                               helpText('Enter Month from 1 to 11'))
+                            ),
+                            conditionalPanel(condition = "input.dataLevel == 'Preprocessed'",
+                              selectInput('varPro', 'Variable', c('Land Cover Type', 'Daymet (Raster)', 'Daymet (Polygon)', 'LST'), selected='Land Cover Type'),
+                              conditionalPanel(condition="input.varPro != 'Land Cover Type'",
+                                               numericInput('month', 'Month', min=1, max=11, value=7),
+                                               helpText('Enter Month from 1 to 11')))
+                            ),
+                          mainPanel(plotOutput('plotout'))),  # Change it to leaflet later
+                 tabPanel('Statistical Analysis',
+                          sidebarPanel(
+                            radioButtons('plot1', 'Result to Plot', c('LST vs. Daymet', 'LST-Daymet Offset by Land Cover Type', 'LST by Land Cover Type in one Daymet Polygon'), selected='LST vs. Daymet'),
+                            conditionalPanel(condition="input.plot1=='LST by Land Cover Type in one Daymet Polygon'",
+                                             helpText('See Daymet Polygon ID in Data Explorer'),
+                                             selectInput('tileID', 'Tile ID',seq(1,900,1),selected=1),  # ---- change this to another wiget more suitable for numbers
+                                             selectInput('tileMon', 'Month', seq(1,11,1), selected=1)
+                            ),
+                            conditionalPanel(condition="input.plot1=='LST vs. Daymet'",
+                                             helpText('This is help text for LST vs. Daymet'),
+                                             selectInput('vsMon', 'Month', seq(1,11,1), selected=1))
+                            ),
+                          mainPanel(plotOutput('plotres'))
+                          )
   
 )
 
---
+++ intro.md
@@ -0,0 +1,25 @@
+Temperature plays a crucial role in the environment, influencing the distribution, behavior, and phenology of diverse species. It also shapes the dynamics and functions of ecosystems. Consequently, comprehending and accurately detecting temperature is of improtance for a thorough understanding of ecological processes.
+
+At present, the primary methods for acquiring temperature data involve **ground observing stations** and **remote sensing technology**. The ground-based observations are limited by spatial resolution and extent, while the remote sensing data are limited by the satellite's revisit time, namely time resolution. By analyzing and comparing these two data sources, with the help of land cover type data, we can enhance our comprehension of disparities between them, which helps further development of upscaling technology aimed at obtaining temperature data of higher temporal and spatial resolution.
+
+## Data Used in this App
+* Daymet Temperature Data
+    * Time Resolution: daily or monthly
+    * Space Resolution: 1km
+    * Temperature Type: air temperature
+    * New York City, Jan-Nov, 2019
+    * Data Source: [Daymet](https://daymet.ornl.gov)<br>
+<br>
+
+* Landsat Land Surface Temperature (LST)
+    * Time Resolution: montly
+    * Space Resolution: 30m
+    * Temperature Type: surface temperature
+    * New York City, Jan-Nov, 2019
+    * Data Source: [Google Earch Engine](https://earthengine.google.com)<br>
+<br>
+
+* Land Cover Type
+    * Space Resolution: 30m
+    * New York City, 2019
+    * Data Source: [USGS](https://www.usgs.gov/centers/eros/science/national-land-cover-database)<br>
